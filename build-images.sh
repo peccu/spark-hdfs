8c71a025ccc14c1310e188ee0248920859e41997
peccu <peccul@gmail.com>
2024-01-21 11:54:55 +0900

    enable hive

--- build-images.sh
+++ build-images.sh
@@ -9,8 +9,8 @@ docker build -t hadoop-base .
 cd ../hdfs
 echo hdfs
 docker build -t hdfs .
-# cd ../hive
-# echo metastore
-# docker build -t metastore -f Dockerfile.metastore .
-# echo hive server
-# docker build -t hive-server -f Dockerfile.server .
+cd ../hive
+echo metastore
+docker build -t metastore -f Dockerfile.metastore .
+echo hive server
+docker build -t hive-server -f Dockerfile.server .
