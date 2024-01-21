fe6b1c7398637e2b776f839124517932fc9fbd60
peccu <peccul@gmail.com>
2024-01-21 15:11:23 +0900

    update reference script

--- docker/jupyterlab/start-jupyter.sh
+++ docker/jupyterlab/start-jupyter.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token=
+jupyter lab \
+        --ip=0.0.0.0 \
+        --port=8888 \
+        --no-browser \
+        --allow-root \
+        --NotebookApp.token=
+
+        # --NotebookApp.tornado_settings='{\"headers\":{\"Content-Security-Policy\":\"frame-ancestors self *YourDomain.com*; report-uri /api/security/csp-report\"}}'"
